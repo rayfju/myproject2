0a18095a6f783bf06a1a22256c682b980b19b400
Ray <ray920707@gmail.com>
2022-03-04 16:10:47 +0800

    scoring

---
+++ src/main/java/com/fju/Scoring.java
@@ -0,0 +1,10 @@
+package com.fju;
+
+public class Scoring {
+    public static void main(String[] args) {
+        Student ivan = new Student();
+        ivan.name = "Ivan";
+        ivan.english = 70;
+        ivan.math = 88;
+    }
+}
---
+++ src/main/java/com/fju/Student.java
@@ -0,0 +1,8 @@
+package com.fju;
+
+public class Student {
+    String name;
+    int english;
+    int math;
+
+}
