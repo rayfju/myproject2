09ef1e4d64fd3b3d2a1e8f07d673a2e16092d674
Ray <ray920707@gmail.com>
2022-03-04 15:29:54 +0800

    scoringNG2

--- src/main/java/com/fju/ScoringNG.java
+++ src/main/java/com/fju/ScoringNG.java
@@ -1,13 +1,17 @@
 package com.fju;
 
 
+
+
 public class ScoringNG {
     public static void main(String[] args) {
         String[] names={"Ivan","Raay","Hank","Apple","Frank"};
         int[] eng={90,60,50,40,10};
         int[] math={60,50,70,90,88};
         for (int i = 0; i < 5; i++) {
-            System.out.println(names[i] + "\t" + eng[i] + "\t" + math[i] + "\t" + (eng[i] + math[i]) / 2);
+            if (i !=3 ) {
+                System.out.println(names[i] + "\t" + eng[i] + "\t" + math[i] + "\t" + (eng[i] + math[i]) / 2);
+            }
         }
     }
 }
