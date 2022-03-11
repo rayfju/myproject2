8e900775060000011fb32563e52667bbc2d253e6
User <ray920707@gmail.com>
2022-03-11 16:26:05 +0800

    constructor and inheritance/super class

--- src/main/java/com/fju/Scoring.java
+++ src/main/java/com/fju/Scoring.java
@@ -2,9 +2,11 @@ package com.fju;
 
 public class Scoring {
     public static void main(String[] args) {
-        Student ivan = new Student();
-        ivan.name = "Ivan";
-        ivan.english = 70;
-        ivan.math = 88;
+        Student ivan = new Student("ivan",70,88);
+        Student fxxk = new Student("fxxk",100,98);
+        GraduateStudent jane = new GraduateStudent("Jane",78,80,77);
+        ivan.print();
+        fxxk.print();
+        jane.print();
     }
 }
