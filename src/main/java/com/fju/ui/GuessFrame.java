0545b7343faf7a649487cfaa95fdf4e459b33dad
Ray <ray920707@gmail.com>
2022-05-27 16:26:11 +0800

    Homework1

--- src/main/java/com/fju/ui/GuessFrame.java
+++ src/main/java/com/fju/ui/GuessFrame.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class GuessFrame extends JFrame {
     public GuessFrame(){
@@ -13,15 +14,26 @@ public class GuessFrame extends JFrame {
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         JButton button=new JButton("HI");
         JLabel label = new JLabel("HELLO");
+        JTextField number=new JTextField(8);
+        int secret=new Random().nextInt(10)+1;
+        System.out.println(secret);
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("Hello");
-                label.setText("oh my god");
+                int num= Integer.parseInt(number.getText());
+                System.out.println(num);
+                if (num>secret){
+                    label.setText("Smaller");
+                }else if(num<secret){
+                    label.setText("Bigger");
+                }else {
+                    label.setText("BINGO");
+                }
+               // label.setText("oh my god");
             }
         });
         setLayout(new FlowLayout());
-
+        add(number);
         add(button);
         add(label);
         setVisible(true);
