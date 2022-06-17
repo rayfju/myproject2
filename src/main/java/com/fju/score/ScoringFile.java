29e9b015c444e946025df20c7ffc4c8dfa7ce73c
Ray <ray920707@gmail.com>
2022-06-17 16:27:58 +0800

    last

--- src/main/java/com/fju/score/ScoringFile.java
+++ src/main/java/com/fju/score/ScoringFile.java
@@ -1,5 +1,6 @@
 package com.fju.score;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -8,10 +9,18 @@ public class ScoringFile {
     public static void main(String[] args) {
         try{
         FileReader fileReader=new FileReader("score.txt");
-        int data = fileReader.read();
-        System.out.println(data);
-        data = fileReader.read();
-        System.out.println(data);
+        BufferedReader reader =new BufferedReader(fileReader);
+        String line=reader.readLine();
+        while (line!=null){
+            System.out.println(line);
+            String[] token= line.split(",");
+            String name = token[0];
+            int english = Integer.parseInt(token[1]);
+            int math =  Integer.parseInt(token[2]);
+            Student stu= new Student(name,english,math);
+            stu.print();
+            line = reader.readLine();
+       }
     }catch(FileNotFoundException e){
             System.out.println("檔案讀取失敗");
         }catch (IOException e){
